fa098a204d38615eda627a432fcc409dee6c5126
marcus.wirtz <marcus.wirtz@rwth-aachen.de>
2020-03-23 12:44:14 +0100

    Catch floating precision in likelihood calculation, improve general plotting

--- fit_data.py
+++ fit_data.py
@@ -2,7 +2,6 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-from scipy.optimize import curve_fit, minimize
 
 from train_model import likelihood
 from models import DayDrivenPandemie
@@ -17,14 +16,11 @@ os.makedirs('img', exist_ok=True)
 
 confirmed_data, dead_data = data.to_numpy()[36:, 0] - 16, data.to_numpy()[36:, 1]
 confirmed_day_data, dead_day_data = np.diff(confirmed_data), np.diff(dead_data)
-mask1, mask2 = confirmed_day_data > 0, dead_day_data > 0
 days = np.arange(len(confirmed_data))
 
 
 def sample_likelihood(pars):
-    infected, infected_confirmed = np.zeros(days.size), np.zeros(days.size)
-    infected_day = np.zeros(days.size)
-    cured, dead = np.zeros(days.size), np.zeros(days.size)
+    infected_confirmed, dead = np.zeros(days.size), np.zeros(days.size)
     world = DayDrivenPandemie(n_days=days.size,
                               n_p=pars[0],
                               attack_rate=0.15,
@@ -36,15 +32,15 @@ def sample_likelihood(pars):
 
     for i in days:
         world.update()
-        infected[i], infected_confirmed[i] = world.infected_total, world.confirmed_total
-        infected_day[i], dead[i] = world.infected_day, world.dead
+        infected_confirmed[i], dead[i] = world.confirmed_total, world.dead
 
-    return -likelihood(np.diff(infected_confirmed)[mask1], confirmed_day_data[mask1], np.diff(dead)[mask2], dead_day_data[mask2])
+    return -likelihood(np.diff(infected_confirmed), confirmed_day_data, np.diff(dead), dead_day_data)
+
 
 lowest_like = np.inf
-n_p_candidates = np.linspace(13, 16, num=10)
-lethality_candidates = np.linspace(1e-3, 0.04, num=10)
-infected_start_candidates = np.linspace(50, 500, num=10)
+n_p_candidates = np.arange(14, 20.5, 0.5)
+lethality_candidates = np.arange(0.005, 0.06, 0.005)
+infected_start_candidates = np.arange(100, 500, 25)
 likelihoods = np.zeros((len(n_p_candidates), len(lethality_candidates), len(infected_start_candidates)))
 for i, n in enumerate(n_p_candidates):
     for j, l in enumerate(lethality_candidates):
@@ -57,23 +53,41 @@ for i, n in enumerate(n_p_candidates):
                 min_idx = (i, j, k)
 
 like_proj = np.min(likelihoods, axis=1)
-fig, ax = plt.subplots(1, 1)
-ax.imshow(np.transpose(like_proj))
-plt.xticks(np.arange(len(n_p_candidates)), np.round(n_p_candidates, 1))
-plt.yticks(np.arange(len(infected_start_candidates)), np.round(infected_start_candidates, 1))
-ax.set_xlabel("np")
-ax.set_ylabel("infected start")
+plt.imshow(np.transpose(like_proj), cmap='inferno_r')
+xticks = np.rint(np.linspace(0, len(n_p_candidates)-1, 6)).astype(int)
+plt.xticks(xticks, np.round(n_p_candidates[xticks], 1))
+yticks = np.rint(np.linspace(0, len(infected_start_candidates)-1, 6)).astype(int)
+plt.yticks(yticks, np.round(infected_start_candidates[yticks], 1))
+plt.xlabel("np")
+plt.ylabel("infected start")
+plt.savefig('img/likelihood_np_is.png', bbox_inches='tight')
+plt.close('all')
 
 like_proj = np.min(likelihoods, axis=2)
-fig, ax = plt.subplots(1, 1)
-ax.imshow(np.transpose(like_proj))
-plt.xticks(np.arange(len(n_p_candidates)), np.round(n_p_candidates, 1))
-plt.yticks(np.arange(len(lethality_candidates)), np.round(lethality_candidates, 4))
-ax.set_xlabel("np")
-ax.set_ylabel("lethality")
-plt.show()
+plt.imshow(np.transpose(like_proj), cmap='inferno_r')
+plt.xticks(xticks, np.round(n_p_candidates[xticks], 1))
+yticks = np.rint(np.linspace(0, len(lethality_candidates)-1, 6)).astype(int)
+plt.yticks(yticks, np.round(lethality_candidates[yticks], 3))
+plt.xlabel("np")
+plt.ylabel("lethality")
+plt.savefig('img/likelihood_np_lethality.png', bbox_inches='tight')
+plt.close('all')
+
+like_proj = np.min(likelihoods, axis=2)
+plt.imshow(np.transpose(like_proj), cmap='inferno_r')
+plt.xticks(xticks, np.round(n_p_candidates[xticks], 1))
+yticks = np.rint(np.linspace(0, len(lethality_candidates)-1, 6)).astype(int)
+plt.yticks(yticks, np.round(lethality_candidates[yticks], 3))
+plt.xlabel("np")
+plt.ylabel("lethality")
+plt.savefig('img/likelihood_is_lethality.png', bbox_inches='tight')
+plt.close('all')
 
 i, j, k = min_idx[0], min_idx[1], min_idx[2]
+print('\nBest parameters:')
+print('np = %.1f' % n_p_candidates[i])
+print('infected start: %.1f' % infected_start_candidates[k])
+print('lethality: %.4f' % lethality_candidates[j])
 pred_len = 40
 days_pred = np.arange(days.size + pred_len)
 infected, infected_confirmed = np.zeros(days_pred.size), np.zeros(days_pred.size)
--- train_model.py
+++ train_model.py
@@ -1,9 +1,9 @@
 import numpy as np
-import scipy as sp
 from scipy.stats import poisson
 
 
 def likelihood(cases, cases_expect, deaths, deaths_expect):
-    likelihood_cases = np.log10(poisson.pmf(cases_expect, mu=cases))
-    likelihood_deaths = np.log10(poisson.pmf(deaths_expect, mu=deaths))
+    # get probability mass function and catch floating precision
+    likelihood_cases = np.log10(np.clip(poisson.pmf(cases_expect, mu=cases), a_min=1e-11, a_max=None))
+    likelihood_deaths = np.log10(np.clip(poisson.pmf(deaths_expect, mu=deaths), a_min=1e-11, a_max=None))
     return np.sum(likelihood_cases) + np.sum(likelihood_deaths)
