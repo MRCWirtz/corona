8be275b0ee34a2565256f983c8b09b1935f8ba57
marcus.wirtz <marcus.wirtz@rwth-aachen.de>
2020-09-17 22:04:26 +0200

    Automatic save/read for DIVI loader

--- intensiv/load_data.py
+++ intensiv/load_data.py
@@ -1,32 +1,53 @@
+import os
+import numpy as np
 import pandas as pd
 import requests
 import datetime
+import matplotlib.pyplot as plt
+from matplotlib import dates
 
+cache, path = True, 'data/intensiv.csv'
 url = 'https://www.divi.de/divi-intensivregister-tagesreport-archiv-csv/'
 
-today = datetime.date.today()
-formats = ["12-15", "12-15-2", "09-15", "09-15-2"]
+today = datetime.datetime.today()
 
-while True:
+if cache and os.path.exists(path):
+    data = pd.read_csv(path)
+    start_day = datetime.datetime.strptime(data['date'].iloc[-1], '%Y-%m-%d')
+else:
+    data = pd.DataFrame([], index=[], columns=['date', 'faelle_covid_aktuell'])
+    start_day = datetime.datetime(2020, 4, 25)
+
+formats = ["12-15", "12-15-2", "09-15", "09-15-2", "14-15", "14-15-2"]
+while start_day <= today:
     for f in formats:
-        _id = '%s-%s' % (today.strftime('%Y-%m-%d'), f)
+        _id = '%s-%s' % (start_day.strftime('%Y-%m-%d'), f)
         r = requests.get(url + 'divi-intensivregister-%s/' % _id)
         value = r.text.split('value="')[1].split('"')[0]
         r = requests.get(url + 'divi-intensivregister-%s/viewdocument/%s' % (_id, value))
         if r.apparent_encoding == 'ascii':
             break
     try:
-        data = pd.read_csv(url + 'divi-intensivregister-%s/viewdocument/%s' % (_id, value))
-        if 'faelle_covid_aktuell' in data:
-            print('(%s) Personen in Intensiv: ' % _id, data['faelle_covid_aktuell'].sum())
+        _data = pd.read_csv(url + 'divi-intensivregister-%s/viewdocument/%s' % (_id, value))
+        if ('faelle_covid_aktuell' in data) and (start_day.strftime('%Y-%m-%d') not in data['date'].values):
+            data = data.append({'date': start_day.strftime('%Y-%m-%d'),
+                                'faelle_covid_aktuell': _data['faelle_covid_aktuell'].sum()}, ignore_index=True)
+            print('(%s) Personen in Intensiv: ' % _id, _data['faelle_covid_aktuell'].sum())
         else:
             break
     except pd.errors.ParserError:
         pass
-    today -= datetime.timedelta(days=1)
+    start_day += datetime.timedelta(days=1)
 
-# https://www.divi.de/divi-intensivregister-tagesreport-archiv-csv/divi-intensivregister-2020-08-26-12-15/viewdocument/5013
-# https://www.divi.de/divi-intensivregister-tagesreport-archiv-csv/divi-intensivregister-2020-08-24-12-15/viewdocument/5008
-# https://www.divi.de/divi-intensivregister-tagesreport-archiv-csv/divi-intensivregister-2020-06-26-12-15/viewdocument/3774
-# https://www.divi.de/divi-intensivregister-tagesreport-archiv-csv/divi-intensivregister-2020-06-25-12-15-2/viewdocument/3919
-# https://www.divi.de/divi-intensivregister-tagesreport-archiv-csv/divi-intensivregister-2020-05-19-09-15/viewdocument/3704
+data.to_csv(path)
+print(data)
+
+plt.plot_date(data['date'], data['faelle_covid_aktuell'], marker='o', color='k')
+plt.gca().xaxis.set_minor_locator(dates.MonthLocator())
+plt.gca().xaxis.set_minor_formatter(dates.DateFormatter('%b'))
+plt.gca().xaxis.set_major_locator(dates.YearLocator())
+plt.gca().xaxis.set_major_formatter(dates.DateFormatter('%Y'))
+plt.ylabel('Covid in Intensivstation (DIVI)', color='k')
+plt.grid(True)
+plt.savefig('img/intensiv.png', bbox_inches='tight')
+plt.close()
